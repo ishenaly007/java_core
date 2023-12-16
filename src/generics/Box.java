54fa5480673a9aef4880843eaa3c93dc5bdff430
ishenaly007 <ishennarkozuev07@gmail.com>
2023-12-16 17:50:52 +0600

    generics

---
+++ src/generics/Box.java
@@ -0,0 +1,31 @@
+package generics;
+
+public class Box<T> { //may extends Number (if you want only nums)
+    private T item;
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public void getDouble(Box<?> asasa){
+        //you may do this when it wants exactly this one type
+        System.out.println(asasa);
+    }
+
+    public static void main(String[] args) {
+        Box<String> stringBox = new Box<>();
+        stringBox.setItem("asas");
+        System.out.println(stringBox.getItem());
+        //stringBox.setItem(12); error
+
+        Box<Integer> integerBox = new Box<>();
+        integerBox.getDouble(stringBox);
+        integerBox.setItem(12);
+        System.out.println(integerBox.getItem());
+        //integerBox.setItem("asas"); error
+    }
+}
