dd7ff331da342887650f6d43cae0fce6f537e8b2
ishenaly007 <ishennarkozuev07@gmail.com>
2024-01-19 14:07:56 +0600

    Times and threads practice

---
+++ src/time_baby/practice/CallableTestTime.java
@@ -0,0 +1,53 @@
+package time_baby.practice;
+
+import multithreading.threads_pull.CallableTest;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class CallableTestTime implements Callable {
+
+    private LocalDateTime eventDate;
+
+    public CallableTestTime(LocalDateTime eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    @Override
+    public String call() throws Exception {
+        long d = 0;
+        while ((d = Duration.between(LocalDateTime.now(), eventDate).toSeconds()) >= 0) {
+            System.out.println("Event " + Thread.currentThread().getName() + "will be started in " + d);
+            Thread.sleep(1000);
+        }
+        return "Event " + Thread.currentThread().getName() + " started";
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime dateTime = LocalDateTime.now();
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+
+        List<Future<String>> futures = new ArrayList<>();
+        for (int i = 1; i <= 4; i++) {
+            LocalDateTime targetTime = dateTime.plusSeconds(5 * i);
+            Callable<String> callable = new CallableTestTime(targetTime);
+            Future<String> future = pool.submit(callable);
+            futures.add(future);
+        }
+
+        for (Future<String> future : futures) {
+            try {
+                System.out.println(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        pool.shutdown();
+    }
+
+}
